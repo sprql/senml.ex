7b182c01a69ae8ba79bf30c812bbc15feba1e9a7
Alexander Obukhov <dev@sprql.space>
2020-02-27 00:04:12 +0300

    Add credo

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule SenML.MixProject do
     [
       {:jason, "~> 1.1"},
       {:cortex, "~> 0.1", only: [:dev, :test]},
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, "~> 1.2", only: [:dev, :test], runtime: false}
     ]
   end
 
